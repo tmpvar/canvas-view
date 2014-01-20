00d58d96c02877a5dac73c12fa66bff34cbb9b20
Elijah Insua <tmpvar@gmail.com>
2014-01-19 20:57:56 -0700

    "initial"

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var canvas-view = require('../canvas-view.js');
+
+var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
+
+describe('canvas-view', function() {
+  describe('#', function() {
+    it('', function() {
+      
+    });
+  });
+});
